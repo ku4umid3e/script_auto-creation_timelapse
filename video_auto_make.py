7a5f6d57a200a46a42d37b3bc88750d3503cf10d
ku4umid3e <kuchumid3e@gmail.com>
2022-02-13 00:41:13 +0300

    I added file deletion feature and folders after creating video

--- video_auto_make.py
+++ video_auto_make.py
@@ -3,38 +3,46 @@ import ffmpeg
 
 
 def get_path():
-    path = input("Please enter the absolute path to the folder with the photo collection\n")
+    path = input(
+        """Please enter the absolute path to
+        the folder with the photo collection\n"""
+        )
     return path
 
 
-def path_to_jpg():
+def path_to_jpg(path):
     folders = []
-    path = os.walk(get_path())
+    route = os.walk(path)
 
-    for dirpath, dirnames, filenames in path:
+    for dirpath, dirnames, filenames in route:
         for dirname in dirnames:
             folders.append(os.path.join(dirpath, dirname))
     return folders
 
 
-def make_video(folders):
+def make_video():
+    path = get_path()
+    folders = path_to_jpg(path)
     for folder in folders:
-
         (
             ffmpeg
             .input(f'{folder}/*.jpg', pattern_type='glob', framerate=30)
             .filter('deflicker', mode='am', size=15)
             .filter('scale', size='hd1080', force_original_aspect_ratio='increase')
-            .output(f"{folder.split('/')[-1]}.mp4")
+            .output(f"data/{folder.split('/')[-1]}.mp4")
             .run()
         )
-    print('Ok')
+        print('Ok')
+        print(">-<"*10)
+        remove_folder(path, folder)
 
 
-def main():
-    make_video(path_to_jpg())
-
+def remove_folder(path, folder):
+    files = os.listdir(folder)
+    for file in files:
+        os.remove(os.path.join(path, folder, file))
+    os.rmdir(folder)
 
 
 if __name__ == '__main__':
-    main()
+    make_video()
