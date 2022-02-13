5d64191c519c906dba03ff26c5334fbdc75c898c
ku4umid3e <kuchumid3e@gmail.com>
2022-02-13 17:17:06 +0300

    A little refactoring

--- video_auto_make.py
+++ video_auto_make.py
@@ -20,25 +20,33 @@ def path_to_jpg(path):
     return folders
 
 
+def video_assembly(folder, path):
+    finished_video = os.path.join(path, "finished_video")
+    if not os.path.isdir(finished_video):
+        os.mkdir(finished_video)
+    (
+        ffmpeg
+        .input(f'{folder}/*.jpg', pattern_type='glob', framerate=30)
+        .filter('deflicker', mode='am', size=15)
+        .filter('scale', size='hd1080', force_original_aspect_ratio='increase')
+        .output(f"{finished_video}/{folder.split('/')[-1]}.mp4")
+        .run()
+    )
+
+
 def make_video():
     path = get_path()
     folders = path_to_jpg(path)
     for folder in folders:
-        (
-            ffmpeg
-            .input(f'{folder}/*.jpg', pattern_type='glob', framerate=30)
-            .filter('deflicker', mode='am', size=15)
-            .filter('scale', size='hd1080', force_original_aspect_ratio='increase')
-            .output(f"data/{folder.split('/')[-1]}.mp4")
-            .run()
-        )
-        print('Ok')
-        print(">-<"*10)
-        remove_folder(path, folder)
+        files = os.listdir(folder)
+        if len(files) > 1000:
+            video_assembly(folder, path)
+            print('Ok')
+            print(">-<"*10)
+            remove_folder(path, folder, files)
 
 
-def remove_folder(path, folder):
-    files = os.listdir(folder)
+def remove_folder(path, folder, files):
     for file in files:
         os.remove(os.path.join(path, folder, file))
     os.rmdir(folder)
